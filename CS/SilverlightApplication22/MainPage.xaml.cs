7150d6d918d73003dff661fc0bfa1235a0aa6f97
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication22/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+// ...
+
+namespace SilverlightApplication22 {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            documentPreview1.Model.CreateDocument();
+        }
+    }
+}
